f61d4bd7ed89136dd242228f75d4a428c84e423e
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2018-04-17 00:21:38 +0200

    Add sidebar menu

--- app/src/main/java/rs/cod3rs/shopifine/activity/ProductsActivity.java
+++ app/src/main/java/rs/cod3rs/shopifine/activity/ProductsActivity.java
@@ -1,7 +1,13 @@
 package rs.cod3rs.shopifine.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import org.androidannotations.annotations.AfterViews;
@@ -15,10 +21,19 @@ import rs.cod3rs.shopifine.adapter.ProductsAdapter;
 import rs.cod3rs.shopifine.domain.Product;
 
 @EActivity(R.layout.activity_products)
-public class ProductsActivity extends AppCompatActivity {
+public class ProductsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @ViewById
+    Toolbar toolbar;
 
     @ViewById
     ListView products;
+
+    @ViewById
+    DrawerLayout drawerLayout;
+
+    @ViewById
+    NavigationView navigationView;
 
     @Bean
     ProductsAdapter adapter;
@@ -28,9 +43,29 @@ public class ProductsActivity extends AppCompatActivity {
         products.setAdapter(adapter);
     }
 
+    @AfterViews
+    void setToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
     @ItemClick
     void productsItemClicked(final Product product) {
         Log.i("AAAAAAA", "AABABABABABABA");
     }
 
+    @AfterViews
+    void setDrawer() {
+        // FIXME Better implementation
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
