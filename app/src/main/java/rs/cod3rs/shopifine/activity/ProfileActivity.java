78c03794c784f4a45f148610f56487cc0fa97089
Bojan Blagojevic <datiglavaradi@gmail.com>
2018-04-18 01:35:05 +0200

    Add Profile Activity

---
+++ app/src/main/java/rs/cod3rs/shopifine/activity/ProfileActivity.java
@@ -0,0 +1,12 @@
+package rs.cod3rs.shopifine.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import rs.cod3rs.shopifine.R;
+
+@EActivity(R.layout.activity_profile)
+public class ProfileActivity extends AppCompatActivity {
+
+}
