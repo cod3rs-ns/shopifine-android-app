6b27e44aeca4a04cd95478d1c1e82895c460a147
Stefan Ristanović <st.keky@gmail.com>
2018-06-06 23:53:19 +0200

    Create wishlist design

---
+++ app/src/main/java/rs/cod3rs/shopifine/fragment/WishlistFragment.java
@@ -0,0 +1,10 @@
+package rs.cod3rs.shopifine.fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import rs.cod3rs.shopifine.R;
+
+@EFragment(R.layout.fragment_wishlist)
+public class WishlistFragment {
+
+}
